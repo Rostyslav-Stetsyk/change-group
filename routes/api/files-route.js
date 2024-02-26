fcb8468f7ad742c41380edbf489956346eae9747
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-02-26 14:41:20 +0200

    init project

---
+++ routes/api/files-route.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const filesRouter = express.Router();
+
+filesRouter.post("/");
+
+export default filesRouter;
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
+import filesRouter from "./routes/api/files-route.js";
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("api/files", filesRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use(({ status = 500, message = "Server error" }, req, res, next) => {
+  res.status(status).json({ message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use port: ${PORT}`);
+});
